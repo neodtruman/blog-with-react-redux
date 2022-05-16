d4a38c3a735e72e3240420aac55969934ad6dc68
Neo D. Truman <neodtruman@gmail.com>
2022-05-16 16:45:52 +0700

    2.3 JSX

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  const name = 'Neo';
+  return (
+    <div className="App">
+      <h1>My First React App</h1>
+      <p>Hello {name}</p>
+    </div>
+  );
+}
+
+export default App;

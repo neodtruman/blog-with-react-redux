01cb3182d85bb33c42aa711bac7e06528b3bced2
Neo D. Truman <neodtruman@gmail.com>
2022-05-17 07:50:55 +0700

    7.2 Reducer

--- src/contexts/user.context.js
+++ src/contexts/user.context.js
@@ -1,12 +1,45 @@
-import { createContext, useState } from "react";
+import { createContext, useReducer } from "react";
 
 export const UserContext = createContext({
   currentUser: null,
   setCurrentUser: () => null
 });
 
+export const USER_ACTION_TYPES = {
+  SET_CURRENT_USER: 'SET_CURRENT_USER'
+}
+
+const userReducer = (state, action) => {
+  console.log('Dispatch action:', action);
+  const { type, payload } = action;
+
+  switch (type) {
+    case USER_ACTION_TYPES.SET_CURRENT_USER:
+      return {
+        ...state,
+        currentUser: payload
+      }
+    default:
+      throw new Error(`Unhandled type ${type} in userReducer`);
+  }
+}
+
+const INITIAL_STATE = {
+  currentUser: null
+}
+
 export const UserProvider = ({ children }) => {
-  const [currentUser, setCurrentUser] = useState(null);
+  const [state, dispatch] = useReducer(userReducer, INITIAL_STATE);
+
+  const { currentUser } = state;
+
+  const setCurrentUser = (user) => {
+    dispatch({
+      type: USER_ACTION_TYPES.SET_CURRENT_USER,
+      payload: user
+    });
+  }
+
   const value = { currentUser, setCurrentUser };
 
   return <UserContext.Provider value={value}>

34c7acd4935c8bff9a74dfd3553969e5ea70d132
Neo D. Truman <neodtruman@gmail.com>
2022-05-17 09:18:43 +0700

    9.2 useMemo - Part 1

--- src/pages/login-page.component.js
+++ src/pages/login-page.component.js
@@ -1,4 +1,4 @@
-import { Fragment, useCallback } from "react";
+import { Fragment, useCallback, useState } from "react";
 import { useDispatch } from "react-redux";
 
 import { setCurrentUser } from "../store/user/user.action";
@@ -6,6 +6,15 @@ import { signInWithGooglePopup } from '../utils/firebase.utils';
 
 const LoginPage = () => {
   const dispatch = useDispatch();
+  const [counterOne, setCounterOne] = useState(0);
+  const [counterTwo, setCounterTwo] = useState(0);
+
+  const doubleCounterOne = useCallback(() => {
+    console.log('doubleCounterOne() called');
+    return counterOne * 2;
+  }, [counterOne]);
+  const c1x2 = doubleCounterOne();
+  console.log('counterOne =', c1x2);
 
   const signInWithGoogle = useCallback(async () => {
     const response = await signInWithGooglePopup();
@@ -15,6 +24,15 @@ const LoginPage = () => {
   return (
     <Fragment>
       <h1>Login Page</h1>
+
+      <div>
+        <span>Counter 1: {counterOne}</span>
+        <button onClick={() => setCounterOne(counterOne + 1)}>Increase Counter 1</button>
+      </div>
+      <div>
+        <span>Counter 2: {counterTwo}</span>
+        <button onClick={() => setCounterTwo(counterTwo + 1)}>Increase Counter 2</button>
+      </div>
 
       <button onClick={signInWithGoogle}>
         Google Sign In

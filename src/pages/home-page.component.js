3230a43206643447c6d214aa6036fbeb20510379
Neo D. Truman <neodtruman@gmail.com>
2022-05-17 08:18:27 +0700

    8.4.4 Show Featured Posts in The HOME Page

--- src/pages/home-page.component.js
+++ src/pages/home-page.component.js
@@ -1,6 +1,35 @@
+import { Fragment, useEffect, useState } from 'react';
+
+import { getFeaturedPosts } from '../utils/firebase.utils';
+
+import PostsGrid from '../components/post/posts-grid.component';
+
 const HomePage = () => {
+  const [posts, setPosts] = useState([]);
+  const [isPending, setIsPending] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    getFeaturedPosts()
+      .then(data => {
+        if (!data || data.length === 0) {
+          setError('Loading data failed!');
+        }
+        else {
+          setPosts(data);
+        }
+      })
+      .catch(err => setError(err.message))
+      .finally(() => setIsPending(false));
+  }, []);
+
   return (
-    <h1>Home Page</h1>
+    <Fragment>
+      <h1>Featured Posts</h1>
+      {isPending && <p>Loading...</p>}
+      {error && <p>{error}</p>}
+      {posts && <PostsGrid posts={posts} />}
+    </Fragment>
   );
 }
 
